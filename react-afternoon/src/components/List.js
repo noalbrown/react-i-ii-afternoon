a5cae2dece3a9dafaaae0fd44dd088489b770336
Noal Brown <noalbrown1@gmail.com>
2020-07-23 18:17:37 -0500

    react

--- react-afternoon/src/components/List.js
+++ react-afternoon/src/components/List.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react'
 class List extends Component {
   render() {
     return (
-      <div>
-        <p>Counter</p>
-        <h1>Name</h1>
-        <div>From:</div>
-        <div>Job Title:</div>
-        <div>Employer:</div>
-        <div>Favorite Movies:
-        <ul>
+      <div className="List">
+        <p id="Counter">Counter</p>
+        <h1 id="Name">Name</h1>
+        <div className="info">From:</div>
+        <div className="info">Job Title:</div>
+        <div className="info">Employer:</div>
+        <div className="info" id="favMovies">Favorite Movies:
+        <ul className="movieList">
             <li>Movie1</li>
             <li>Movie2</li>
             <li>Movie3</li>
